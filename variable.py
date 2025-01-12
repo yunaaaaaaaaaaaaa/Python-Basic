507c418aa9483acab72c28b5a65cd77cd5ba7a03
yunaaaaaaaaaaaaa <superyuna13@gmail.com>
2025-01-12 02:58:10 +0000

    20250112

---
+++ variable.py
@@ -0,0 +1,63 @@
+# variable
+# variable = value
+# Save the value in variable format above.
+# = Unlike the meaning of the symbol, the value is simply stored in a variable.
+# Variable can be whatever you want as long as you follow the rules below.
+# 1. Impossible to start with a number
+# 2. Special characters are not possible except the symbol "_"
+# 3. No spacing 
+# 4. Reserved words are not possible 
+
+# my_name = "Yuna"
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Data types
+# str   -> String   => made up of quotes " "  ' '
+# float -> Float    => no quotes and has a decimal
+# int   -> Integer  => no quotes and no decimal point
+# Everyday words (English, Chinese, Korean, etc.) without quotes => error
+
+# a = 10      # saves the integer 10 in the variable a
+# b = 'cit'   # saves the string 'cit' in the variable b
+
+# name = 'Yuna'
+# age = 12
+# height = 147
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# print('text' or variable or number)
+# The print() function prints the 'text', number, or value of 
+# the variable inside the bracket and then adds a newline.
+# To print multiple items, use a comma (,)
+# If you use print() with nothing inside, ir prints an empty line
+
+# name = 'Yuna'
+# age = 12
+# height = 147
+# print(name)
+# print() # prints an empty line
+# print(age)
+# print(height)
+# print('hi')
+# print()
+# print(5)
+# print(5*10)
+# print(name, age, height, "hello")
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Problem
+# Create two variable(name, school).
+# Make it so that the following content is displayed when run.
+
+name = 'Yuna'
+school = 'YISS'
+print("Hello", "my", "name", "is", name)
+print("I", "am", "currently", "a", "5th-grade", "student", "at",school)
