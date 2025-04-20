67337e4891788458cd52f4216ea8fd66909586e2
yunaaaaaaaaaaaaa <superyuna13@gmail.com>
2025-04-20 02:49:49 +0000

    20250420

--- loop.py
+++ loop.py
@@ -28,5 +28,132 @@
 #     print(i)
 
 # Print perfect squares up to 10000 print(x*x)
-for i in range(1, 101, 1) :           #101
-    print(i*i)
+# for i in range(1, 101, 1) :           #101
+#     print(i*i)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# while loop
+# while(condition) :
+#       code
+# Used in this fromat
+# Similar to if statements, executes if the condition is True
+
+# i = 0
+# while(i < 5) :
+#     print(i)
+#     i += 1                  # i = 1 +1, increment i
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Print even number from 1 to 100
+# i = 2
+# while(i <= 100) :
+#     print(i)
+#     i += 2
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Print  multiples of 7 from 7 to 100
+# i = 7
+# while(i < 100) :
+#     print(i)
+#     i += 7
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Print decimals from 1 to 100
+# i = 1.0
+# while(i <= 100) :
+#     print(1/i)
+#     i += 1                  # I just did print(i) when I was supposed to do print(1/i)
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Infinite loop
+# while(True)
+#       code
+# This creates an infinite loop
+
+# continue and break
+# continue and break can be used in both for and while loops
+
+# continue 
+# when contintue is encountered, the loop moves to the next iteration 
+
+# for i in range(0, 10, 1) :
+#     if(i % 2 == 0) :
+#         continue          # skip even numbers
+#     print(i)
+
+# break
+# when break is encountered, the loop stops
+# break is usually used with if
+
+# i = 0
+# while(True) :
+#     print(i)
+#     if(i == 10) :
+#         break
+#     i += 1
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Sample of using infiinite loop, continue, break
+# while(True) :
+#     print("Please choose a menu option:")
+#     print("1. Print hello")
+#     print("2. Print hi")
+#     print("3. Exit")
+#     menu = int(input())
+
+#     if(menu == 1) :
+#         print("hello")
+#     elif(menu == 2) :
+#         print("hi")
+#     elif(menu == 3) :
+#         print("Exiting the menu program.")
+#         break
+#     else:
+#         print("Invalid input. Please enter a number between 1 and 3.")
+#         print()
+#         print()
+#         continue
+
+#     print()
+#     print()
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Nested loops
+# A loop inside another loop
+# Two nested loops are called double nested, three nested loops are called triple nested
+# More than triple nested loops are rarely used.
+
+# for x in range(1, 10, 1) :              # repeats 9 times
+#     for y in range(1, 10, 1) :          # repeats 81 times in total
+#         print(x, "*", y, "=", x * y)
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# while() multiplication table
+# x = 1
+# while(x < 10) :
+#     y = 1
+#     while(y < 10) :
+#         print(x, "*", y, "=", x * y )
+#         y += 1
+#     x +=1
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# for x in range(1, 10, 1) :
+#     for y in range(1, x+1, 1) :     # Can use an external variable in range
+#         print(x, "*", y, "=", x * y)
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Create a 1-19 multiplication table
+for x in range(1, 20, 1) :
+    for y in range(1, 10, 1) :
+        print(x, "*", y, "=", x * y)
